c5cb7af2c320b43b037de062e1635d8462025f8b
nobody <nobody@6cfa91a5-b114-0410-aa58-ce7c4e7541e0>
2004-01-09 16:00:00 +0000

    This commit was manufactured by cvs2svn to create tag 'Bourbon'.

---
+++ tags/Bourbon/docbook/relaxng/tools/cleanup.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl -- # -*- Perl -*-
+
+# Simple script to cleanup the output of the stylesheets. It fiddles with
+# spuriouis namespace declarations, mostly. And adds newlines between the
+# definitions.
+
+while (<>) {
+    s/<rng:/</g;
+    s/<\/rng:/<\//g;
+    s/\s+xmlns:rng=([\"\']).*?\1\s+/ /g;
+    s/\s*xmlns:rng=([\"\']).*?\1\s*//g;
+
+    s/\s+xmlns:ctrl=([\"\']).*?\1\s+/ /g;
+    s/\s*xmlns:ctrl=([\"\']).*?\1\s*//g;
+
+    s/\s+xmlns:s=([\"\']).*?\1\s+/ /g;
+    s/\s*xmlns:s=([\"\']).*?\1\s*//g;
+
+    s/<(s:rule\s+.*?)>/<\1 xmlns:s=\"http:\/\/www.ascc.net\/xml\/schematron\">/g;
+    s/<(ctrl:\S+\s+.*?)(\/?>)/<\1 xmlns:ctrl=\"http:\/\/nwalsh.com\/xmlns\/schema-control\/\"\2/g;
+
+    print $_;
+    print "\n" if /<\/define>/ || /<ctrl:/ || /<\/start>/;
+}
