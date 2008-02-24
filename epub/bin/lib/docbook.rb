57484cba6a826de9f4eb531fa19663370eecc58c
abdelazer <abdelazer@6cfa91a5-b114-0410-aa58-ce7c4e7541e0>
2008-02-24 20:09:56 +0000

    Basic .epub creation now happening (though missing a ton of files and all of the referenced images)

--- epub/bin/lib/docbook.rb
+++ epub/bin/lib/docbook.rb
@@ -3,25 +3,30 @@ module DocBook
     CHECKER = "epubcheck"
     STYLESHEET = File.expand_path(File.join(File.dirname(__FILE__), '..', '..', "docbook.xsl"))
     XSLT_PROCESSOR = "xsltproc"
-    OUTPUT_DIR = ".epubtmp/"
+    OUTPUT_DIR = ".epubtmp"
+    MIMETYPE = "application/epub+zip"
+    META_DIR = "META-INF"
+    OEBPS_DIR = "OEBPS"
+    ZIPPER = "zip"
 
     attr_reader :output_dir
 
     def initialize(docbook_file, output_dir=OUTPUT_DIR)
       @docbook_file = docbook_file
       @output_dir = output_dir
+      @meta_dir  = File.join(@output_dir, META_DIR)
+      @oebps_dir = File.join(@output_dir, OEBPS_DIR)
+      @to_delete = []
+
       unless File.exist?(@docbook_file)
         raise ArgumentError.new("File #{@docbook_file} does not exist")
       end
     end
 
     def render_to_file(output_file, verbose=false)
-      chunk_quietly = verbose ? '0' : '1'
-      cmd = "#{XSLT_PROCESSOR} --xinclude --stringparam chunk.quietly #{chunk_quietly} --stringparam base.dir #{@output_dir} #{STYLESHEET} #{@docbook_file}"
-      STDERR.puts cmd if $DEBUG
-      success = system(cmd)
-      raise "Could not render as .epub to #{output_file}" unless success
-      File.open(output_file, "w") {|f| f.puts }
+      render_to_epub(output_file, verbose)
+      bundle_epub(output_file, verbose)
+      cleanup_files(@to_delete)
     end
 
     def self.invalid?(file)
@@ -36,5 +41,46 @@ module DocBook
         return output
       end  
     end
+
+    private
+    def render_to_epub(output_file, verbose)  
+      chunk_quietly =  "--stringparam chunk.quietly " + (verbose ? '0' : '1')
+      base =  "--stringparam base.dir #{@oebps_dir}/" 
+      meta =  "--stringparam epub.metainf.dir #{@meta_dir}/" 
+      oebps =  "--stringparam epub.oebps.dir #{@oebps_dir}/" 
+      options = "--xinclude #{chunk_quietly} #{base} #{meta} #{oebps}"
+      db2epub_cmd = "#{XSLT_PROCESSOR} #{options} #{STYLESHEET} #{@docbook_file}"
+      STDERR.puts db2epub_cmd if $DEBUG
+      success = system(db2epub_cmd)
+      raise "Could not render as .epub to #{output_file}" unless success
+      @to_delete << Dir["#{@meta_dir}/*"]
+      @to_delete << Dir["#{@oebps_dir}/*"]
+    end  
+
+    def bundle_epub(output_file, verbose)  
+      quiet = verbose ? "" : "-q"
+      # zip -X -r ../book.epub mimetype META-INF OEBPS
+      mimetype_filename = write_mimetype()
+      meta  = File.basename(@meta_dir)
+      oebps  = File.basename(@oebps_dir)
+      zip_cmd = "cd #{@output_dir} &&  #{ZIPPER} #{quiet} -X -r  #{output_file} #{mimetype_filename} #{meta} #{oebps}"
+      puts zip_cmd if $DEBUG
+      success = system(zip_cmd)
+      raise "Could not bundle into .epub file to #{output_file}" unless success
+    end
+
+    def write_mimetype
+      mimetype_filename = File.join(@output_dir, "mimetype")
+      File.open(mimetype_filename, "w") {|f| f.print MIMETYPE}
+      @to_delete << mimetype_filename
+      return File.basename(mimetype_filename)
+    end  
+
+    def cleanup_files(file_list)
+      file_list.flatten.each {|f|
+        File.delete(f)
+      }  
+    end  
+
   end
 end
