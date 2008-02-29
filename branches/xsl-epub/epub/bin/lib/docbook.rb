4dbe67e98bc85ccf3481c610a79795e25adfb4c9
abdelazer <abdelazer@6cfa91a5-b114-0410-aa58-ce7c4e7541e0>
2008-02-29 01:41:33 +0000

    Added a smoktest target to the specs (they don't all work yet), and better testing in the regular specs
    DocBook::Epub
    - should be able to be created
    - should fail on a nonexistent file
    - should be able to render to a file
    - should create a file after rendering
    - should have the correct mimetype after rendering
    - should be valid .epub after rendering an article
    - should be valid .epub after rendering an article without sections
    - should be valid .epub after rendering a book
    - should be valid .epub after rendering a book even if it has graphics
    - should report an empty file as invalid
    - should confirm that a valid .epub file is valid

--- branches/xsl-epub/epub/bin/lib/docbook.rb
+++ branches/xsl-epub/epub/bin/lib/docbook.rb
@@ -1,4 +1,7 @@
+require 'rexml/parsers/pullparser'
+require 'fileutils'
 module DocBook
+
   class Epub
     CHECKER = "epubcheck"
     STYLESHEET = File.expand_path(File.join(File.dirname(__FILE__), '..', '..', "docbook.xsl"))
@@ -63,10 +66,28 @@ module DocBook
       mimetype_filename = write_mimetype()
       meta  = File.basename(@meta_dir)
       oebps  = File.basename(@oebps_dir)
-      zip_cmd = "cd #{@output_dir} &&  #{ZIPPER} #{quiet} -X -r  #{output_file} #{mimetype_filename} #{meta} #{oebps}"
+      images = copy_images()
+      zip_cmd = "cd #{@output_dir} &&  #{ZIPPER} #{quiet} -X -r  #{output_file} #{mimetype_filename} #{meta} #{oebps} #{images}"
       puts zip_cmd if $DEBUG
       success = system(zip_cmd)
       raise "Could not bundle into .epub file to #{output_file}" unless success
+    end
+
+    def copy_images
+      image_references = get_image_refs()
+      new_images = []
+      image_references.each {|img|
+        img_new_filename = File.join(@oebps_dir, img)
+        img_full = File.join(File.expand_path(File.dirname(@docbook_file)), img)
+
+        # TODO: What to rescue for these two?
+        FileUtils.mkdir_p(File.dirname(img_new_filename)) 
+        puts(img_full + ": " + img_new_filename) if $DEBUG
+        FileUtils.cp(img_full, img_new_filename)
+        @to_delete << img_new_filename
+        new_images << img_full
+      }  
+      return new_images
     end
 
     def write_mimetype
@@ -78,9 +99,22 @@ module DocBook
 
     def cleanup_files(file_list)
       file_list.flatten.each {|f|
-        File.delete(f)
+        # Yikes
+        FileUtils.rm_r(f, :force => true )
       }  
     end  
 
+    # Returns an Array of all of the (image) @filerefs in a document
+    def get_image_refs
+      parser = REXML::Parsers::PullParser.new(File.new(@docbook_file))
+      image_refs = []
+      while parser.has_next?
+        el = parser.pull
+        if el.start_element? and (el[0] == "imagedata" or el[0] == "graphic")
+          image_refs << el[1]['fileref'] 
+        end  
+      end
+      return image_refs
+    end  
   end
 end
