50875984fe25e0eb125136ad85b61ecd1354d938
abdelazer <abdelazer@6cfa91a5-b114-0410-aa58-ce7c4e7541e0>
2008-03-01 01:16:52 +0000

    Drop @width from XHTML 1.1 and we will get farther in the smoketests

--- branches/xsl-epub/epub/bin/spec/epub_smoketest_spec.rb
+++ branches/xsl-epub/epub/bin/spec/epub_smoketest_spec.rb
@@ -18,14 +18,14 @@ describe DocBook::Epub do
   before(:all) do
     @testdocsdir = File.expand_path(File.join(File.dirname(__FILE__), 'testdocs'))
     @tmpdir = File.join(Dir::tmpdir(), "epubspec"); Dir.mkdir(@tmpdir)
-    $DEBUG = false
+    $DEBUG = true
   end
 
   it "should be able to render a valid .epub for each test document in the DocBook testdocs repo" do
     #Dir["#{@testdocsdir}/author.001.xml"].each {|xml_file|
     Dir["#{@testdocsdir}/*.xml"].each {|xml_file|
       epub = DocBook::Epub.new(xml_file, @tmpdir)
-      epub_file = File.join(@tmpdir, "smoketest.epub")
+      epub_file = File.join(@tmpdir, File.basename(xml_file, ".xml") + ".epub")
       epub.render_to_file(epub_file, $DEBUG)
       FileUtils.copy(epub_file, ".smt.epub") if $DEBUG
       epub_file.should be_valid_epub  
